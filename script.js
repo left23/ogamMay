30c264be54e803a0137b20a96fe31b54a8ece76c
left23 <lcdemc@gmail.com>
2014-05-12 21:27:27 +0100

    added first-attempt game loop

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-$( document ).ready(function() {
+$(document).on('ready',function(){
+    setInterval(whaka,500);
+  });
 	
-
-	$("button").click(function() {
+	function whaka(){
 		var mole = $(".mole").toArray();
 		var elemlength = mole.length;
 		var randomnum = Math.floor(Math.random()*elemlength);
 		var randomitem = mole[randomnum];
-		$(randomitem).css("display", "block").addClass("active");		
-	});
+		//$(randomitem).css("display", "block").addClass("active");
+		$(randomitem).removeClass("explode").toggle().toggleClass("active");		
+	}
 	
 	
+$(document).ready(function(){
+
+	//$("#container").css( 'cursor', 'url(mallet.png), auto' );
+
 	$(".mole").click(function() {
-	
+	//alert("!! whaka !!");
 		if ($(this).hasClass("active")) {
-			$(this).css("display", "none").removeClass("active");
+			$(this).css("display","none").addClass("explode");
 		}
 	});
-
+	
 });
+
+
+
