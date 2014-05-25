ebcbc851c5139f9d800a5a6dc69e1520fdc02a47
left23 <lcdemc@gmail.com>
2014-05-25 13:47:20 +0100

    refactor, explode working

--- script.js
+++ script.js
@@ -2,13 +2,18 @@ $(document).on('ready',function(){
     setInterval(whaka,500);
   });
 	
+	
+			var score=0;
+
+
+
 	function whaka(){
 		var mole = $(".mole").toArray();
 		var elemlength = mole.length;
 		var randomnum = Math.floor(Math.random()*elemlength);
 		var randomitem = mole[randomnum];
 		//$(randomitem).css("display", "block").addClass("active");
-		$(randomitem).removeClass("explode").toggle().toggleClass("active");		
+		$(randomitem).removeClass("explode").toggle().toggleClass("active");
 	}
 	
 	
@@ -18,8 +23,12 @@ $(document).ready(function(){
 
 	$(".mole").click(function() {
 	//alert("!! whaka !!");
+	score = score+1;
+	
+	$(".score span").html(score);
+	
 		if ($(this).hasClass("active")) {
-			$(this).css("display","none").addClass("explode");
+			$(this).addClass("explode").delay( 800 ).hide(0);
 		}
 	});
 	
